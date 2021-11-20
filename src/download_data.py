36d27bab479b64da753111d5e05f20d215b61184
Kyle Ahn <KyleA@priceindustries.com>
2021-11-20 14:54:49 -0800

    Edited gitignore for data so all the csv files are ignored

--- src/download_data.py
+++ src/download_data.py
@@ -1,6 +1,6 @@
 """Downloads a csv data from the url to a local file path.
 Usage: download_data.py --url=<url> --file_path=<file_path> 
- 
+
 Options:
 --url=<url>             URL for the CSV file to be downloaded
 --file_path=<file_path>   Local file path to be saved
@@ -15,7 +15,8 @@ opt = docopt(__doc__)
 def main(url, file_path):
     data = pd.read_csv(url, header=None)
     try:
-        if os.path.exists(file_path) is False:
+        folder_path = file_path.rsplit("/", 1)[0]
+        if os.path.exists(folder_path) is False:
             os.makedirs(os.path.dirname(file_path))
         data.to_csv(file_path, index=False)
     except Exception as e: 
