d349cbafc69070532340df6944e0684141edcd4d
Kyle Ahn <KyleA@priceindustries.com>
2021-11-24 21:43:33 -0800

    Add sample-docopt.r which the team can use as base to read & write csv file in R

---
+++ src/sample-docopt.r
@@ -0,0 +1,33 @@
+"Cleans, splits and pre-processes offender profile data from http://www.csc-scc.gc.ca/005/opendata-donneesouvertes/Open%20Data%20File%2020170409%20v3%20(English).csv.
+Usage: src/sample_docopt.r --raw_data_path=<raw_data_path> --processed_dir_path=<processed_dir_path>
+  
+Options:
+--raw_data_path=<raw_data_path>             Path to the raw data including the file name and extension to raw data
+--processed_dir_path=<processed_dir_path>   Path to where the processed data should be written.
+" -> doc
+
+library(tidyverse)
+library(docopt)
+set.seed(2020)
+
+opt <- docopt(doc)
+
+main <- function(raw_data_path, processed_dir_path) {
+    # read data and convert class to factor
+    raw_data <- readr::read_csv(raw_data_path)
+
+    print(raw_data)
+    # write scale factor to a file
+    try({
+        dir.create(processed_dir_path, showWarnings = FALSE)
+    })
+
+    z <- paste(processed_dir_path, "/processed_offender_profile.csv", sep = "")
+    print(z)
+    write.csv(
+        raw_data,
+        paste(processed_dir_path, "/processed_offender_profile.csv", sep = "")
+    )
+}
+
+main(opt[["--raw_data_path"]], opt[["--processed_dir_path"]])
